5613a06236f839dbab65f147b03f237a3b6cc79b
Mathys Vanheulle <mathys19706980@gmail.com>
2025-11-26 12:10:40 +0100

    Correction index.php

--- public/index.php
+++ public/index.php
@@ -1,27 +1,38 @@
+
 <?php
-require '../../vendor/autoload.php';
-require '../../app/config/db.php';
+require '../vendor/autoload.php';
+require '../app/config/db.php';
+use Smarty\Smarty;
 
-// 1. Configuration de Smarty
-$smarty = new Smarty();
-$smarty->setTemplateDir('../../app/views/templates');
-$smarty->setCompileDir('../../tmp/templates_c');
+// -----------------------------------------------------------
+// CONFIGURATION SMARTY & FLIGHT
+// -----------------------------------------------------------
 
-// 2. Configuration de Flight pour utiliser Smarty
-Flight::register('view', 'Smarty', [], function($smarty) {
-    return $smarty;
+// 1. On enregistre Smarty dans Flight.
+// IMPORTANT : On doit utiliser 'Smarty\Smarty' et non juste 'Smarty'
+Flight::register('view', 'Smarty\Smarty', [], function($smarty) {
+    // On configure l'objet Smarty une fois qu'il est créé par Flight
+    $smarty->setTemplateDir('../app/views/templates');
+    $smarty->setCompileDir('../tmp/templates_c');
+    // On peut ajouter d'autres configs ici si besoin (cache, config, etc.)
 });
 
+// 2. On crée une méthode "render" simplifiée pour appeler la vue
 Flight::map('render', function($template, $data){
     Flight::view()->assign($data);
     Flight::view()->display($template);
 });
 
-// 3. Vos Routes
+// -----------------------------------------------------------
+// ROUTES
+// -----------------------------------------------------------
+
 Flight::route('/', function(){
     Flight::render('accueil.tpl', ['nom' => 'Equipe W']);
 });
 
-// 4. Démarrage
+// -----------------------------------------------------------
+// DÉMARRAGE
+// -----------------------------------------------------------
 Flight::start();
 ?>
